8a2cce9dfcc50ed98626ab336d6ca25f7c658038
VBuser <timur.lihgt@rambler.ru>
2024-06-28 10:02:39 +0900

    Добавил решение Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно
+// одновременно 7 и 23
+// Пример:
+// a=50 => нет
+// a=7 => нет
+// a=322 => да
+
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0 && number % 23 == 0 && number > 0)
+{
+    System.Console.WriteLine($"a={number} => Да");
+}
+else
+{
+    System.Console.WriteLine($"a={number} => Нет");
+}
+
